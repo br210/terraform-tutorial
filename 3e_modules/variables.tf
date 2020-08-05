207e95ec5a0e16a9aab97776a47b88af28d3f53e
root <root@ip-172-31-89-108.ec2.internal>
2020-08-05 06:32:18 +0000

    modules

--- 3e_modules/variables.tf
+++ 3e_modules/variables.tf
@@ -1,3 +1,7 @@
-variable "aws_access_key" {}
+variable "aws_access_key" {
+default="AKIAUQ233CKGZLIJ4SHT"
+}
 
-variable "aws_secret_key" {}
+variable "aws_secret_key" {
+default="BHg9Q1jUYav4FfkRC4TMEiOrORa5nYUosReJDOwj"
+}
